5843a304eb8c0c8f7821820fbbebee94fd816949
ritualFire <jpelaez456@gmail.com>
2023-02-16 23:53:13 +0000

    GuessingGame

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,8 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+
+        boolean check = (guessNumber == secretNumber);
+        return check;
     }
 }
